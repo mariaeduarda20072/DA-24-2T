312d96e83afa80e46808812caf59a8d5328c6200
Beelzebub3 <davinascimentojobs@gmail.com>
2024-08-19 17:19:18 -0300

    aula 6

---
+++ aulas/aula 6/aula 6.js
@@ -0,0 +1,158 @@
+/*aula 6 operadores aritméticos
+exemplo de adiçao
+const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor + segundoValor + 4
+
+console.log("O resultado é:",resultado)*/
+
+/*exemplo de subtraçao 
+const primeiroValor = 10
+const segundoValor = 20 
+
+const resultado = primeiroValor - segundoValor 
+
+console.log("O resultado é:",resultado)*/
+
+/*exemplo de multiplicaçao
+const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor + segundoValor + 4
+
+console.log("O resultado é:",resultado)
+
+const resultado = primeiroValor * segundoValor
+
+console.log("O resultado é:",resultado)*/
+
+/*exemplo de divisão
+const primeiroValor = 345
+const segundoValor = 10
+
+const resultado = primeiroValor / segundoValor
+
+console.log("O resultado é:",resultado)*/
+
+/*exemplo resto da divisao
+const restoDivisao = 11%4 
+
+console.log("O resto da divisao é:",restoDivisao)*/
+
+/*exercicio
+atividade 1
+const primeiroValor = 3
+const segundoValor = 4
+
+const resultado = primeiroValor + segundoValor
+
+console.log("O resultado é:",resultado)
+valor:7
+
+atividade 2
+const primeiroValor = 3
+const segundoValor = 5
+
+const resultado = primeiroValor * segundoValor /2
+
+console.log(resultado)
+valor:7,5
+
+atividade 3
+const primeiroValor = 5
+const segundoValor = 4
+
+const resultado = primeiroValor - segundoValor *-1
+
+console.log(resultado)
+valor:9*/
+
+/*atividade 4
+const primeiroValor = 234
+const segundoValor = 5
+
+const resultado = primeiroValor / segundoValor
+
+console.log(resultado)
+
+const restoDivisao = 46%8
+
+console.log(restoDivisao)
+valor:6*/
+
+/*exemplos comparadores
+
+comparador igual === 
+
+comparaçao de valor e tipo 
+
+const condiçao = 1 === 1
+console.log(condiçao)//resultado true
+
+const condiçao2 = 1 === "1"
+console.log(condiçao2)//rsultado false*/
+
+/*comparador diferente !==
+
+const condiçao =1 !== "1"
+console.log(condiçao)//resultado true 
+
+const condiçao2 = 1 !== 1 
+console.log(condiçao2)//reaultado false 
+
+const condiçao3 = 2 !== 1
+console.log(condiçao3)//resultado true*/
+
+/*comparador maior, maior e igual
+
+const condiçao = 1 > 2 
+console.log(condiçao)//resultado false 
+
+const condiçao1 = 2 > 2
+console.log(condiçao1)//resultado false
+
+const condiçao2 = 2 >= 2 
+console.log(condiçao2)//resultado true
+
+const condiçao3 = 3 > 2
+console.log(condiçao3)//resultado true*/
+
+/*comparador menor, menor e igual
+
+const condiçao = 1 < 2
+console.log(condiçao)//resultado true 
+
+const condiçao1 = 2 < 2
+console.log(condiçao1)//resultado false 
+
+const condiçao2 = 2 <= 2
+console.log(condiçao2)//resultado true
+
+const condiçao3 = 3 < 2 
+console.log(condiçao3)//resultado false*/
+
+/*exercicio
+const primeiroValor =2
+const segundoValor =4 
+
+const condicao1 = primeiroValor === segundoValor
+console.log("o primeiro numero é igual ao segundo?",condicao1)//resultado false
+
+const condicao2 = primeiroValor !== segundoValor
+console.log("o primeiro numero é diferente do segundo?",condicao2)//resultado true
+
+const condicao3 = primeiroValor > segundoValor
+console.log("o primeiro numero é maior que o segundo?",condicao3)//resultado false
+
+const condicao4 = primeiroValor < segundoValor
+console.log("o primeiro numero é menor que o segundo?",condicao4)//resultado true*/
+
+
+
+
+
+
+
+
+
