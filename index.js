7f05a55dc334840c6e9f3d00853acd5d3cb7fc77
maria <mariaeduardapazruas@gmail.com>
2024-08-22 17:25:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+//exercicio 1
+
+/*const texto1 = "ola mundo"
+const texto2 = 'ola mundo'
+const texto3 = `ola mundo`
+
+const nome = "maria"
+const idade = 17
+//exemplo de concatenaçao
+//const frase = "o meu nome e: " + nome + " e minha idade e: " + idade + " anos"
+
+//exemplo de template strings
+const frase = `meu nome e: ${nome} e minha idade e: ${idade} anos`
+
+console.log(frase)*/
+
+//exemplo length
+
+/*const nome = 'Maria Eduarda'
+
+console.log(nome.length)//13
+
+//exemplo toLowerCase
+
+console.log(nome.toLowerCase())
+
+//exemplo toUpperCase
+console.log(nome.toUpperCase())*/
+
+/*//exemplo trim
+const email = " mariaeduardapazruas@gmail.com "
+
+console.log(email.trim())*/
+
+//exemplo includes
+/*const frase = "hoje comi cenoura"
+
+console.log(frase.includes("cenoura"))
+
+console.log(frase.includes("batata"))*/
+
+/*exemplo replaceAll
+
+const frase = "hoje comi cenoura,adoro cenoura"
+
+const novaFrase = frase.replaceAll ("cenoura","batata")
+
+console.log(novaFrase)*/
+
+//exercicio 1
+
+/*const frase = prompt("digite uma frase")//ola tudo bem
+
+console.log(frase.toUpperCase())//OLA TUDO BEM
+
+const novaFrase = frase.replaceAll ("o","i")//ila tudi bem
+
+console.log(novaFrase)
+
+console.log(frase.length)//12*/
+
+//exemplo de array
+
+/*const frutas = ["abacate","banana","tomate","milho","uva","morango"]
+
+const fruta = frutas [4]
+
+console.log(fruta)*/
+
+//exercicio 1 array
+
+/*const raças = ["golden retriever","rottweiler","yorkshire","shih tzu","pug"]
+
+const raça = Number(prompt("escolha um numero de 0 a 4"))
+
+console.log(raças[raça])//2 yorkshire*/
+
+//exemplo propriedade length
+
+/*const pokemons = ["bulbasaur","squirtle","charmander","pikachu"]
+
+console.log(pokemons.length)*/
+
+//exemplo metodo includes
+
+/*const seriesBoas = ["vikings","the bing bang theory"]
+
+console.log(seriesBoas.includes ("vikings"))//true
+
+console.log(seriesBoas.includes ("game of Thrones"))//false*/
+
+//exemplo metodo push
+
+/*const numeros = [1,2,3]
+
+numeros.push(4)
+
+console.log(numeros)
+
+numeros.push(5,6,7,8)
+
+console.log(numeros)*/
+
+//exemplo de metodo pop
+
+/*const meusPeixes = ["palhaço","mandarim","esturjao"]
+
+meusPeixes.pop()
+
+console.log(meusPeixes)*/
+
+//exemplo metodo splice
+
+const letras = ["A","B","C","D","E","F","G","H"]
+
+letras.splice(2,1)
+console.log(letras)
+
+letras.splice(5,2)
+console.log(letras)
+
+
